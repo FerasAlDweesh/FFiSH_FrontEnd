dc634ad8ac1203bd9d99608b8ced2784a8281527
Feras AlDweesh <ferasaldweesh@gmail.com>
2019-12-28 22:55:32 +0300

    styling edited, put in seperate folder, addter rating bar

--- stores/pointStore.js
+++ stores/pointStore.js
@@ -1,7 +1,9 @@
 import { decorate, observable } from "mobx";
 import { instance } from "./instance";
+import vendorStore from "./vendorStore";
 class PointStore {
   points = [];
+  userPoints = [];
   loading = true;
 
   addPoint = async cardID => {
@@ -10,11 +12,22 @@ class PointStore {
       const res = await instance.post("points/", card);
       const points = res.data;
       console.log("Errorrrrrr", points);
-      this.points = points;
+      this.userPoints = points % vendorStore.vendorCards.points;
       this.loading = false;
       console.log("LOADINGNNG", this.loading);
     } catch (err) {
       console.log("ERROR", err);
+    }
+  };
+
+  fetchUserPoints = async () => {
+    try {
+      const res = await instance.get("points/");
+      const points = res.data;
+      this.points = points;
+      this.loading = false;
+    } catch (err) {
+      console.log(err);
     }
   };
 }
